0a73314d8959afea4e0c38258eb0f7019fcf62b0
Jim Coble <jim.coble@duke.edu>
2017-07-05 09:52:31 -0400

    Add lib to autoload paths and clean up active job adapter configs.

--- config/application.rb
+++ config/application.rb
@@ -10,20 +10,20 @@ module Scholrax
   class Application < Rails::Application
 
 
-      # The compile method (default in tinymce-rails 4.5.2) doesn't work when also
-      # using tinymce-rails-imageupload, so revert to the :copy method
-      # https://github.com/spohlenz/tinymce-rails/issues/183
-      config.tinymce.install = :copy
+    # The compile method (default in tinymce-rails 4.5.2) doesn't work when also
+    # using tinymce-rails-imageupload, so revert to the :copy method
+    # https://github.com/spohlenz/tinymce-rails/issues/183
+    config.tinymce.install = :copy
     config.generators do |g|
       g.test_framework :rspec, :spec => true
     end
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.active_job.queue_adapter = :inline
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Load environment variables from file
     # http://railsapps.github.io/rails-environment-variables.html
