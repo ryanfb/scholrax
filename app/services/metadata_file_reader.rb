a7063a361957c70f2c510b9b85e4c7e9339598d8
ad96 <ad96@duke.edu>
2017-07-25 15:46:31 -0400

    Fall back to only element lookup if element-and-qualifier are not found. (#9)

--- app/services/metadata_file_reader.rb
+++ app/services/metadata_file_reader.rb
@@ -3,20 +3,25 @@ class MetadataFileReader
   DEFAULT_METADATA_MAPPING_FILE = Rails.root.join('config', 'metadata_mapping.yml')
 
   attr_reader :metadata_doc, :metadata_mapping_file
+  attr_accessor :metadata
 
   def initialize(metadata_file_path, metadata_mapping_file = DEFAULT_METADATA_MAPPING_FILE)
     @metadata_doc = read_metadata_file(metadata_file_path)
     @metadata_mapping_file = metadata_mapping_file
+    @metadata = {}
   end
 
   def call
-    metadata = {}
     nodes = metadata_doc.xpath('//dcvalue')
     nodes.each do |node|
-      property_key = metadata_map[node['element'] + '|' + node['qualifier']]
-      if property_key
-        metadata[property_key] ||= []
-        metadata[property_key] << node.text
+      if node.text
+        property_key = metadata_map[node['element'] + '|' + node['qualifier']]
+        if property_key
+          set_metadata_value(property_key, node.text)
+        else
+          property_key = metadata_map[node['element']]
+          set_metadata_value(property_key, node.text) if property_key
+        end
       end
     end
     metadata
@@ -28,6 +33,11 @@ class MetadataFileReader
 
   def metadata_map
     YAML.load(File.read(metadata_mapping_file))
+  end
+
+  def set_metadata_value(key, value)
+    metadata[key] ||= []
+    metadata[key] << value
   end
 
 end
--- spec/services/metadata_file_reader_spec.rb
+++ spec/services/metadata_file_reader_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe MetadataFileReader do
     let(:expected_hash) { { 'creator' => [ 'Adams, A', 'Barnes, B'],
                             'based_near' => [ 'England' ],
                             'date_created' => [ '2017-07-10T21:37:32Z' ],
-                            'identifier' => [ 'https://www.ncbi.nlm.nih.gov/pubmed/999999', 'abc.012.12345' ] } }
+                            'identifier' => [ 'https://www.ncbi.nlm.nih.gov/pubmed/999999', 'abc.012.12345' ],
+                            'title' => [ 'Cats Are Happiness' ] } }
     subject { described_class.new(metadata_file_path, metadata_mapping_file) }
     it "returns the expected hash of metadata" do
       expect(subject.call).to match(expected_hash)
