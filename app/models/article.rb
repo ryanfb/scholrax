fdf1330ad86c665bbf441e5d6bd222816802f41b
Ryan Baumann <ryan.baumann@gmail.com>
2017-09-28 12:16:21 -0400

    Add custom properties to Article work type

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,26 @@ class Article < ActiveFedora::Base
 
   self.human_readable_type = 'Article'
 
+  property :institution, predicate: ::RDF::URI.new('http://id.loc.gov/vocabulary/relators/dgg'), multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :publication_date, predicate: ::RDF::Vocab::DC.issued, multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :doi, predicate: ::RDF::URI.new('http://id.loc.gov/vocabulary/identifiers/doi'), multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  # property :license, predicate: ::RDF::Vocab::DC.license, multiple: false do |index|
+  #   index.as :stored_searchable
+  # end
+
+  property :abstract, predicate: ::RDF::Vocab::DC.abstract, multiple: false do |index|
+    index.as :stored_searchable
+  end
+
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
